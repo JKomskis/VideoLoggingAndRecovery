0e6b963be80a8d0a4ab3fc3fce8c50d9a61228be
Isaac Weintraub <weintraubisaac@gmail.com>
2021-05-02 04:44:25 +0000

    fix profiling method

--- test/benchmark/abstract_profile.py
+++ test/benchmark/abstract_profile.py
@@ -20,20 +20,20 @@ class AbstractProfile(ABC):
     def _tearDown(self):
         pass
 
-def run_profile(self):
-    LoggingManager().log(f'Running set up for {self.profile_name}', LoggingLevel.INFO)
-    self._setUp()
-    with Profile() as prof:
-        self._run()
+    def run_profile(self):
+        LoggingManager().log(f'Running set up for {self.profile_name}', LoggingLevel.INFO)
+        self._setUp()
+        with Profile() as prof:
+            self._run()
 
-    profile_dump = f'{BENCHMARK_DATA_FOLDER}/{self.profile_name}.prof'
-    prof.dump_stats(profile_dump)
+        profile_dump = f'{BENCHMARK_DATA_FOLDER}/{self.profile_name}.prof'
+        prof.dump_stats(profile_dump)
 
-    LoggingManager().log(f'Running tear down for {self.profile_name}', LoggingLevel.INFO)
-    self._tearDown()
+        LoggingManager().log(f'Running tear down for {self.profile_name}', LoggingLevel.INFO)
+        self._tearDown()
 
-    LoggingManager().log(f'Creating flamegraph of {self.profile_name}', LoggingLevel.INFO)
-    # flameprof only has a documented command line interface
-    svg = run([executable, 'flameprof.py', profile_dump], stdout=PIPE)
-    with open(f'{BENCHMARK_DATA_FOLDER}/{self.profile_name}.svg', 'w') as svg_file:
-        svg_file.write(svg)
+        LoggingManager().log(f'Creating flamegraph of {self.profile_name}', LoggingLevel.INFO)
+        # flameprof only has a documented command line interface
+        svg = run(['flameprof', profile_dump], stdout=PIPE)
+        with open(f'{BENCHMARK_DATA_FOLDER}/{self.profile_name}.svg', 'w') as svg_file:
+            svg_file.write(svg.stdout.decode('utf8'))
